456757ef8ca47a3dd8e42fe7d34b0e13d2a29c1b
ha819914 <ha819914@gmail.com>
2022-03-13 01:03:42 +0900

    Added menemanager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,7 +2,11 @@
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("1. Add Money");
+		System.out.println("2. Delete Money");
+		System.out.println("3. Edit Money");
+		System.out.println("4. View Money");
+		System.out.println("5. Exit");
 	}
 
 }
